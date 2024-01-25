8439b717438a90f0f9b3e257be200e8c2a717ea2
Sarah Van Bogaert <sarahvbogaert@gmail.com>
2024-01-25 14:35:46 +0100

    shorter names in views.py

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -7,20 +7,20 @@ from basic_app import forms
 def register(request):
 	if request.method == "POST":
 		user_form = forms.UserForm(request.POST)
-		user_profile_info_form = forms.UserProfileInfoForm(request.POST, files=request.FILES)
-		if user_form.is_valid() and user_profile_info_form.is_valid():
+		profile_form = forms.UserProfileInfoForm(request.POST, files=request.FILES)
+		if user_form.is_valid() and profile_form.is_valid():
 			user = user_form.save()
 			user.set_password(user.password)
 			user.save()
-			profile = user_profile_info_form.save(commit = False)
+			profile = profile_form.save(commit = False)
 			profile.user = user
-			user_profile_info_form.save()
+			profile_form.save()
 			print(profile.profile_pic)
 			return render(request, "basic_app/register_success.html")
 	else:
 		user_form = forms.UserForm()
-		user_profile_info_form = forms.UserProfileInfoForm()
-	context_dict = {'user_form': user_form, 'user_profile_info_form': user_profile_info_form}
+		profile_form = forms.UserProfileInfoForm()
+	context_dict = {'user_form': user_form, 'profile_form': profile_form}
 	return render(request, "basic_app/register_form.html", context_dict)
 
 @login_required
